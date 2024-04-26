047fe94dc5c23596651a52310296fac977391edd
shirasuke <shirasu.ry@kanamekey.com>
2024-04-26 09:38:38 +0900

    add database connect

---
+++ connect/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+type Mydata struct{
+	ID int
+	Name string
+	Mail string
+	Age int
+}
+
+func (m *Mydata) Str() string{
+	return "<\""+strconv.Itoa(m.ID)+":"+m.Name+"\""+m.Mail+","+strconv.Itoa(m.Age)+">"
+} 
+
+func main() {
+	  dsn :="host=localhost port=5432 dbname=postgres user=postgres password=postgres sslmode=disable"
+    db, err := sql.Open("postgres", dsn)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+}
